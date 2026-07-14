5628201fbb97b01193a461972a676ede1b82ba99
Marc-Andre Dufour <bleeband.dev@gmail.com>
2026-07-14 12:24:29 -0400

    ADD: Ajout du middleware

---
+++ backend/src/middlewares/auth.middleware.ts
@@ -0,0 +1,35 @@
+import { type Request, type Response, type NextFunction } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+import jwt from "jsonwebtoken";
+
+export function authentifier(req: Request, res: Response, next: NextFunction) {
+  const header = req.headers.authorization;
+
+  if (!header?.startsWith("Bearer ")) {
+    return res.status(401).json({ erreur: "Token manquant" });
+  }
+
+  const token = header.split(" ")[1];
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET!);
+
+    (req as any).user = payload;
+
+    next();
+  } catch {
+    return res.status(401).json({ erreur: "Token invalide / expire" });
+  }
+}
+
+export function exigerRole(role: string) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if ((req as any).user.role !== role) {
+      return res.status(403).json({ erreur: "Acces refuse" });
+    }
+
+    next();
+  };
+}
